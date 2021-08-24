459e912666d4c7c0707df582edc01924dc00f9f6
victorkolis <victorkolis@gmail.com>
2021-08-24 17:09:32 -0300

    NOTHINGBURGER

---
+++ Courses/DerekBanas/COMMENTS.lisp
@@ -0,0 +1,13 @@
+;;;; This is a clisp comment to describe the program normally the only tiem to use it
+
+;;; This is the more common way commenting a code in Lisp
+
+;; Comment indented with code
+
+; Comment after a line code
+
+#||
+Multiline comment
+||#
+
+
---
+++ Courses/DerekBanas/OUTPUT_TEXT.lisp
@@ -0,0 +1,7 @@
+;;;; This software outputs a string to the console
+
+(format t "Hello Lisp ~%") ; `t` indicates the terminal usage, `~%` is a newline
+
+;;; printing out to the console with print
+(print "This is being printed with (print )")
+
